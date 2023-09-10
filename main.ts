49fc9f5be48ac65588f4b0c3ce6bfc5a9622e536
Joe Constant <joe@joeconstant.com>
2023-09-10 17:06:54 -0600

    Initial update of name/description/etc...

--- main.ts
+++ main.ts
@@ -1,62 +1,53 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, ButtonComponent, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface OQSyncSettings {
+	clientId: string;
+	clientSecret: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: OQSyncSettings = {
+	clientId: '',
+	clientSecret: '',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class OQSync extends Plugin {
+	settings: OQSyncSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('sync', 'OQSync', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('Synced with Quire!');
 		});
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('oqsync-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('OQSync status: ???');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'auth-with-quire',
+			name: 'Authenticate with Quire',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				new QuireAuthModal(this.app).open();
 			}
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'sync-with-quire',
+			name: 'Sync with Quire',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
+				const hasAuthToken = false; //TODO: Add check to see if we have an auth token
+				if (hasAuthToken) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new QuireSyncModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,7 +57,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new OQSyncSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -91,7 +82,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class QuireSyncModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -107,10 +98,26 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class QuireAuthModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
 
-	constructor(app: App, plugin: MyPlugin) {
+	onOpen() {
+		const {contentEl} = this;
+		contentEl.setText('Woah!');
+	}
+
+	onClose() {
+		const {contentEl} = this;
+		contentEl.empty();
+	}
+}
+
+class OQSyncSettingTab extends PluginSettingTab {
+	plugin: OQSync;
+
+	constructor(app: App, plugin: OQSync) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +128,29 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setHeading()
+			.setName('Quire API Settings')
+			.setDesc('Create these at https://quire.io/apps/dev')
+			.addButton((cb) => cb.setButtonText('Register app with Quire').onClick(() => window.open('https://quire.io/apps/dev')));
+
+		new Setting(containerEl)
+			.setName('Client ID')
+			.setDesc('Client ID from Quire App (can be Development credentials)')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your client id')
+				.setValue(this.plugin.settings.clientId)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.clientId = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Client Secret')
+			.setDesc('Client Secret from Quire App (can be Development credentials)')
+			.addText(text => text
+				.setPlaceholder('Enter your client secret')
+				.setValue(this.plugin.settings.clientSecret)
+				.onChange(async (value) => {
+					this.plugin.settings.clientSecret = value;
 					await this.plugin.saveSettings();
 				}));
 	}
